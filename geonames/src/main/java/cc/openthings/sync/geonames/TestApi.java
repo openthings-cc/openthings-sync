8628c651bd2fca91d18ffb9ac368be1c3f41ed66
Kalin Maldzhanski <djodjo@djodjo.org>
2015-01-15 00:30:40 +0100

    add some tests on geonames api

--- geonames/src/main/java/cc/openthings/sync/geonames/TestApi.java
+++ geonames/src/main/java/cc/openthings/sync/geonames/TestApi.java
@@ -15,10 +15,40 @@
  */
 
 package cc.openthings.sync.geonames;
+import cc.openthings.sender.HttpResponse;
+import cc.openthings.sender.HttpSender;
+
+import java.io.IOException;
 
 public class TestApi {
 
     public static void main(String[] args) {
+        HttpResponse httpResponse;
+        try {
+            //httpResponse = doSearch();
+            httpResponse = doWikiBBSearch();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HttpResponse doSearch() throws IOException {
+        return new HttpSender(Config.baseUrl + Config.uriSearch
+                + Config.defParams
+                + Config.getBoundigboxParams(54.1,4.4,5.2,55.2)
+                + "&featureCode=ans" )
+                .setMethod(HttpSender.GET)
+                .doCall();
+    }
+    private static HttpResponse doWikiBBSearch() throws IOException {
+        return new HttpSender(Config.baseUrl + Config.uriWikiBBSearch
+                + Config.defParams
+                + Config.getBoundigboxParams(54.1,4.4,5.2,55.2)
+               //does not apply :(
+               // + "&featureCode=ans"
+        )
+                .setMethod(HttpSender.GET)
+                .doCall();
     }
 }
