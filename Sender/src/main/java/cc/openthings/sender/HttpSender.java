1246fc0ae859787bf7b411732d01072b6abb01d7
Kalin Maldzhanski <djodjo@djodjo.org>
2015-05-18 20:15:41 +0200

    get some beer

--- Sender/src/main/java/cc/openthings/sender/HttpSender.java
+++ Sender/src/main/java/cc/openthings/sender/HttpSender.java
@@ -29,133 +29,140 @@ import java.util.Map;
 
 public class HttpSender {
 
-	private final String USER_AGENT = "openthingsSync/1.0";
-	public static String POST = "POST";
-	public static String GET = "GET";
-	public static String PUT = "PUT";
-	public static String DELETE = "DELETE";
+    private final String USER_AGENT = "openthingsSync/1.0";
+    public static String POST = "POST";
+    public static String GET = "GET";
+    public static String PUT = "PUT";
+    public static String DELETE = "DELETE";
 
-	HttpURLConnection conn;
+    HttpURLConnection conn;
 
-	public HttpSender(String url) {
-		try {
-			conn = (HttpURLConnection)new URL(url).openConnection();
-			conn.setUseCaches(false);
+    public HttpSender(String url) {
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setUseCaches(false);
             conn.setConnectTimeout(5000);
             conn.setReadTimeout(50000);
-			conn.setRequestProperty("User-Agent", USER_AGENT);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            conn.setRequestProperty("User-Agent", USER_AGENT);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public HttpSender addHeaders(Map headers) {
-		Iterator it = headers.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, String> entry = (Map.Entry<String, String>)it.next();
-			addHeader(entry.getKey(), entry.getValue());
-		}
-		return this;
-	}
+    public HttpSender addHeaders(Map headers) {
+        Iterator it = headers.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
+            addHeader(entry.getKey(), entry.getValue());
+        }
+        return this;
+    }
 
-	public HttpSender addHeader(String key, String value) {
-		conn.addRequestProperty(key, value);
-		return this;
-	}
+    public HttpSender addHeader(String key, String value) {
+        conn.addRequestProperty(key, value);
+        return this;
+    }
 
-	public HttpResponse doCall() throws IOException {
-		return doCall(null);
-	}
+    public HttpResponse doCall() throws IOException {
+        return doCall(null);
+    }
 
-	public HttpResponse doCall(String body) throws IOException {
+    public HttpResponse doCall(String body) throws IOException {
 
-		if(body==null) {
-			System.out.println("Call with NO body.");
-			conn.connect();
-		} else {
-			System.out.println("Call with body:");
-			System.out.println(body);
-			conn.setDoOutput(true);
-			byte[] bytes = body.getBytes();
-			if(!conn.getRequestMethod().equals(DELETE)) {
-				OutputStream out = conn.getOutputStream();
-				try {
-					out.write(bytes);
-				} finally {
-					close(out);
-				}
-			}
-		}
-
-		BufferedReader in;
-		if(conn.getResponseCode()>299) {
-			in = new BufferedReader(
-					new InputStreamReader(conn.getErrorStream()));
-		} else {
-			in = new BufferedReader(
-					new InputStreamReader(conn.getInputStream()));
-		}
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		System.out.println(conn.getResponseCode());
-		System.out.println(response.toString().replace("</br>", "\n").replace("<br>", "\n").replace("</p>", "</p>\n"));
-		System.out.println(getOutHeaders(conn.getHeaderFields()));
-		return new HttpResponse(conn.getResponseCode(), response.toString(), conn.getHeaderFields());
-	}
+        if (body == null) {
+            System.out.println("Call with NO body.");
+            conn.connect();
+        } else {
+            System.out.println("Call with body:");
+            System.out.println(body);
+            conn.setDoOutput(true);
+            byte[] bytes = body.getBytes();
+            if (!conn.getRequestMethod().equals(DELETE)) {
+                OutputStream out = conn.getOutputStream();
+                try {
+                    out.write(bytes);
+                } finally {
+                    close(out);
+                }
+            }
+        }
 
 
-	public HttpSender setMethod(String httpMethod) {
-		try {
-			conn.setRequestMethod(httpMethod);
-		} catch (ProtocolException e) {
-			e.printStackTrace();
-		}
-		return this;
-	}
+        StringBuffer response = new StringBuffer();
+        System.out.println(conn.getResponseCode());
+        try {
+            BufferedReader in;
 
-	private static void close(Closeable closeable) {
-		if (closeable != null) {
-			try {
-				closeable.close();
-			} catch (IOException e) {
-				// ignore error
-				//logger.log(Level.FINEST, "IOException closing stream", e);
-			}
-		}
-	}
+            if (conn.getResponseCode() > 299) {
+                in = new BufferedReader(
+                        new InputStreamReader(conn.getErrorStream()));
+            } else {
+                in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+            }
+            String inputLine;
 
-	public void close() {
-		if(conn==null) return;
-		try {
-			conn.disconnect();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
 
-	public static String getOutHeaders(Map<String, List<String>> headers) throws IOException {
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
 
-		StringBuffer response = new StringBuffer();
-		Iterator<Map.Entry<String, List<String>>> it = headers.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, List<String>> entry = it.next();
-			response.append(entry.getKey() + ": ");
-			for(String se:entry.getValue()) {
-				response.append(se);
-			}
-			response.append("\n");
-		}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(response.toString().replace("</br>", "\n").replace("<br>", "\n").replace("</p>", "</p>\n"));
+        //System.out.println(getOutHeaders(conn.getHeaderFields()));
+        return new HttpResponse(conn.getResponseCode(), response.toString(), conn.getHeaderFields());
+    }
 
-		return response.toString();
 
-	}
+    public HttpSender setMethod(String httpMethod) {
+        try {
+            conn.setRequestMethod(httpMethod);
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    private static void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                // ignore error
+                //logger.log(Level.FINEST, "IOException closing stream", e);
+            }
+        }
+    }
+
+    public void close() {
+        if (conn == null) return;
+        try {
+            conn.disconnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getOutHeaders(Map<String, List<String>> headers) throws IOException {
+
+        StringBuffer response = new StringBuffer();
+        Iterator<Map.Entry<String, List<String>>> it = headers.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, List<String>> entry = it.next();
+            response.append(entry.getKey() + ": ");
+            for (String se : entry.getValue()) {
+                response.append(se);
+            }
+            response.append("\n");
+        }
+
+        return response.toString();
+
+    }
 
 }
---
+++ beermapping/src/main/java/cc/openthings/sync/beermapping/Config.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 OpenThings Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.openthings.sync.beermapping;
+
+//info from http://www.geonames.org/export/ws-overview.html
+public class Config {
+
+
+    public static final String userString = "c6266a50b6603fe87d681ef34fe11e3e";
+    public static final String baseUrl = "http://beermapping.com/webservice/";
+    //get general info
+    public static final String locquery = "locquery";
+
+    //get lat/lon
+    public static final String locmap = "locmap";
+
+    //get ratings
+    public static final String locscore = "locscore";
+
+    //get pics
+    public static final String locimage = "locimage";
+
+
+}
