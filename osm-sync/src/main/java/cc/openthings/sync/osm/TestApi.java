36775d31a3843ae7269ec8e513035566d948963c
Kalin Maldzhanski <djodjo@djodjo.org>
2015-06-08 03:32:30 +0200

    add some tests

--- osm-sync/src/main/java/cc/openthings/sync/osm/TestApi.java
+++ osm-sync/src/main/java/cc/openthings/sync/osm/TestApi.java
@@ -33,8 +33,8 @@ public final class TestApi {
     public static void main(String[] args) {
         HttpResponse httpResponse;
         try {
-            httpResponse = doDummySearch("[building]", Config.QueryType.way);
-            if(httpResponse!=null) {
+            httpResponse = doDummySearch("[route]", Config.QueryType.way);
+            if (httpResponse != null) {
                 System.out.println(httpResponse.body);
             }
             OsmMapper mapper = new OsmMapper();
@@ -42,9 +42,15 @@ public final class TestApi {
             JsonArray elements = JsonElement.readFrom(httpResponse.body).asJsonObject().getJsonArray("elements");
             JsonArray mappedEls = new JsonArray();
             JsonArray nolabelEls = new JsonArray();
-            for(JsonElement el:elements) {
+
+            JsonArray nodes = elements.get(0).asJsonObject().getJsonArray("nodes");
+            for (JsonElement el : nodes) {
+                httpResponse = doIdSearch(el.asLong(), Config.QueryType.node);
+            }
+            System.exit(0);
+            for (JsonElement el : elements) {
                 JsonObject mappedEl = (JsonObject) mapper.getThing(el);
-                if(mappedEl!=null) {
+                if (mappedEl != null) {
                     System.out.println(mappedEl);
                     mappedEls.put(mappedEl);
                     if (!mappedEl.has("http://www.w3.org/2000/01/rdf-schema#label")) {
@@ -63,10 +69,10 @@ public final class TestApi {
     }
 
     private static HttpResponse doDummySearch(String kv, Config.QueryType qType) throws IOException {
-        ArrayList<String> kvs =  new ArrayList<>();
+        ArrayList<String> kvs = new ArrayList<>();
         kvs.add(kv);
-       // -0.42228282889341606,-0.2471923828125
-      //  0.42228316416045586,0.24719204753637317
+        // -0.42228282889341606,-0.2471923828125
+        //  0.42228316416045586,0.24719204753637317
         return doSearch(kvs, new BoundingBox(48.29892057002853, 7.485171295702457, 48.8576793564343, 7.9795560613274565), qType);
     }
 
@@ -75,8 +81,7 @@ public final class TestApi {
         try {
             System.out.println("overpass created request: " + kvs + ", bounds: " + bounds);
             return new HttpSender(Config.osmOverpassBaseUrl
-                    + Config.buildNodeQuery(kvs, bounds))
-                    //+ Config.buildQuery(kvs, bounds, qType))
+                    + Config.buildQuery(kvs, bounds, qType))
                     .setMethod(HttpSender.GET)
                     .doCall();
 
@@ -89,8 +94,20 @@ public final class TestApi {
         // Config.osmOverpassBaseUrl + "node" + ((featureCode==null)?"":"["+featureCode+"]") + "(" + Math.min(bounds.northeast.latitude, bounds.southwest.latitude)+","+Math.min(bounds.northeast.longitude, bounds.southwest.longitude)+","+Math.max(bounds.northeast.latitude, bounds.southwest.latitude)+","+ Math.max(bounds.northeast.longitude, bounds.southwest.longitude)+ ");" + "out%20500;"
         //Config.osmOverpassBaseUrl + "node" + ((featureCode==null)?"[tourism]":"["+featureCode+"]") + "(" + Math.min(bounds.northeast.latitude, bounds.southwest.latitude)+","+Math.min(bounds.northeast.longitude, bounds.southwest.longitude)+","+Math.max(bounds.northeast.latitude, bounds.southwest.latitude)+","+ Math.max(bounds.northeast.longitude, bounds.southwest.longitude)+ ");" + "out%20500;"
         //Config.osmOverpassBaseUrl + "node" + "[tourism]" + "(" + Math.min(bounds.northeast.latitude, bounds.southwest.latitude)+","+Math.min(bounds.northeast.longitude, bounds.southwest.longitude)+","+Math.max(bounds.northeast.latitude, bounds.southwest.latitude)+","+ Math.max(bounds.northeast.longitude, bounds.southwest.longitude)+ ");" + "out%205;"
-
-
     }
 
+    public static HttpResponse doIdSearch(long id, Config.QueryType qType) {
+        try {
+            System.out.println("overpass created ID request: " + id + ", qType: " + qType);
+            return new HttpSender(Config.osmOverpassBaseUrl
+                    + Config.buildIdQuery(id, qType))
+                    .setMethod(HttpSender.GET)
+                    .doCall();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
 }
