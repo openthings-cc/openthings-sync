f31d2696339c9af6e8fd70df4089f699b4b042d4
Kalin Maldzhanski <djodjo@djodjo.org>
2015-06-18 18:33:51 +0200

    add villo and velo urls

---
+++ rental-bikes/src/main/java/cc/openthings/sync/rentalbikes/villo/Config.java
@@ -0,0 +1,10 @@
+package cc.openthings.sync.rentalbikes.villo;
+
+public class Config {
+
+    private Config() {
+
+    }
+
+    public static final String baseUrl = "http://www.mobielbrussel.irisnet.be/villo/json/";
+}
---
+++ rental-bikes/src/main/java/cc/openthings/sync/rentalbikes/villo/package-info.java
@@ -0,0 +1,3 @@
+
+package cc.openthings.sync.rentalbikes.villo;
+//brussels bike rentals
